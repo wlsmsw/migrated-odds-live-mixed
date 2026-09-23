1d259547368a7581b7dc2bcd46349621e2834c10
imsteff <stphnencomienda30@gmail.com>
2026-09-23 11:08:22 +0800

    ISB-1825: turn off mysqli exception reporting so a bad odds row skips instead of killing the cron

--- config.php
+++ config.php
@@ -147,6 +147,7 @@ class Config
 
       try 
       {
+         mysqli_report(MYSQLI_REPORT_OFF); // for PHP 8.1+ which throws on query failure
          $this->db_conn = new mysqli('p:'.$this->db_host, $this->db_username, $this->db_password, $this->db_name);
          if (mysqli_connect_errno())
          {
